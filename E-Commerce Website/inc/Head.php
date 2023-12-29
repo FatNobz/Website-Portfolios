f5f238048c436d8c1d076b9b56d56085926209e2
FatNobz <115465795+FatNobz@users.noreply.github.com>
2023-12-29 21:34:18 +0000

    Adding Web Files
    Check these fuckers out bby

---
+++ E-Commerce Website/inc/Head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imperious Competitions - Massive Designer Competitions</title>
+    <link rel="shortcut icon" href="Images/Icon/Favicon.ico" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" />
+    <link rel="stylesheet" href="Style.css">
+</head>
