524e5ba3a29f1b26a9259cde11ad4f9fc1511faf
cc8838 <117163552+cc8838@users.noreply.github.com>
2024-01-19 18:01:53 +0000

    Blue Lagoon

--- TheBlueLagoon/Script.js
+++ TheBlueLagoon/Script.js
@@ -1,9 +1,11 @@
 function showOneWayForm() {
     document.getElementById('departureSection').style.display = 'block';
     document.getElementById('returnSection').style.display = 'none';
-  }
-  
-  function showReturnForm() {
+    document.getElementById('submitButton').style.display = 'block';
+}
+
+function showReturnForm() {
     document.getElementById('departureSection').style.display = 'none';
     document.getElementById('returnSection').style.display = 'block';
-  }
+    document.getElementById('submitButton').style.display = 'block';
+}
