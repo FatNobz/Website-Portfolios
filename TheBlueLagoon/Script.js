e54d50196707a8bbf6ffb90710777c51f8d82178
cc8838 <117163552+cc8838@users.noreply.github.com>
2024-01-19 18:19:42 +0000

    Blue Lagoon

--- TheBlueLagoon/Script.js
+++ TheBlueLagoon/Script.js
@@ -2,10 +2,18 @@ function showOneWayForm() {
     document.getElementById('departureSection').style.display = 'block';
     document.getElementById('returnSection').style.display = 'none';
     document.getElementById('submitButton').style.display = 'block';
+
+    // Make the "Return" button inactive
+    document.getElementById('returnButton').style.opacity = '0.5';
+    document.getElementById('oneWayButton').style.opacity = '1';
 }
 
 function showReturnForm() {
     document.getElementById('departureSection').style.display = 'none';
     document.getElementById('returnSection').style.display = 'block';
     document.getElementById('submitButton').style.display = 'block';
+
+    // Make the "One Way" button inactive
+    document.getElementById('oneWayButton').style.opacity = '0.5';
+    document.getElementById('returnButton').style.opacity = '1';
 }
